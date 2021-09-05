fc121b3f898ea537298c04e7efba8128f6024d7f
widetrace <widetrace@icloud.com>
2021-09-05 20:29:36 +0300

    update vk-bot

--- blackhawksbot-vk/index.js
+++ blackhawksbot-vk/index.js
@@ -1,9 +1,15 @@
 const Vk = require('node-vk-bot-api');
 const Markup = require('node-vk-bot-api/lib/markup');
-const axios = require('axios');
-const moment = require('moment');
 
 const token = require('./config');
+
+const Match = require('../modules/Match');
+const Formatter = require('../modules/Formatter');
+const Processor = require('../modules/Processor');
+
+const match = new Match();
+const formatter = new Formatter();
+const processor = new Processor();
 
 const bot = new Vk(token);
 
@@ -19,75 +25,16 @@ bot.command('/матч', (ctx) => {
   }
 });
 
-bot.command('Следующий матч', (ctx) => {
-  try {
-    axios
-      .get(
-        'https://statsapi.web.nhl.com/api/v1/teams/16?expand=team.schedule.next',
-      )
-      .then((response) => {
-        const game = response.data.teams[0].nextGameSchedule.dates[0].games[0];
-        const gameTime = moment(game.gameDate).format('DD.MM.YY HH:mm');
-        ctx.reply(
-          `&#127954; ${game.teams.home.team.name} сыграют с ${
-            game.teams.away.team.name
-          } в ${game.venue.name}
-          &#128197; Дата: ${gameTime.split(' ')[0]}
-          &#128368; Время (по МСК): ${gameTime.split(' ')[1]} \n 
-          &#128250; Посмотреть онлайн можно здесь: 
-            • onhockey.tv
-            • vk.cc/a8C2JS
-          `,
-          null,
-          matchKeyboard,
-        );
-      });
-  } catch (err) {
-    console.log(err);
-  }
+bot.command('Следующий матч', async (ctx) => {
+  const answer = await processor.sumInfo(match, formatter, 'next');
+
+  ctx.reply(answer, null, matchKeyboard);
 });
 
-bot.command('Предыдущий матч', (ctx) => {
-  let answer = '';
-  try {
-    axios
-      .get(
-        'https://statsapi.web.nhl.com/api/v1/teams/16?expand=team.schedule.previous',
-      )
-      .then((response) => {
-        const responseData = response.data.teams[0].previousGameSchedule.dates[0].games[0];
-        answer = `&#127954; ${responseData.teams.home.team.name} ${
-          responseData.teams.home.score > responseData.teams.away.score
-            ? 'победили'
-            : 'уступили'
-        } ${responseData.teams.away.team.name} \n`;
-        answer
-          += `Счет: ${responseData.teams.home.score}:${responseData.teams.away.score} \n \n`;
-        axios
-          .get(`https://statsapi.web.nhl.com${responseData.link}`)
-          .then((res) => {
-            const gameReview = res.data.liveData;
-            gameReview.plays.scoringPlays.forEach((play) => {
-              const obsPlay = gameReview.plays.allPlays[play];
-              answer
-                += `${obsPlay.about.goals.home}:${obsPlay.about.goals.away} — ${
-                  obsPlay.players[0].player.fullName
-                } (${obsPlay.players[0].seasonTotal})${
-                  obsPlay.result.description.split('assists: ')[1].length > 0
-                    ? `; Передачи: ${
-                      obsPlay.result.description.split('assists: ')[1]
-                    }`
-                    : ' '
-                }\n`;
-            });
-            answer
-              += `\n3 &#11088; матча:\n\n&#11088;${gameReview.decisions.firstStar.fullName};\n&#11088;&#11088;${gameReview.decisions.secondStar.fullName};\n&#11088;&#11088;&#11088;${gameReview.decisions.thirdStar.fullName}.`;
-            ctx.reply(answer, null, matchKeyboard);
-          });
-      });
-  } catch (error) {
-    console.log(error);
-  }
+bot.command('Предыдущий матч', async (ctx) => {
+  const answer = await processor.sumInfo(match, formatter, 'previous');
+
+  ctx.reply(answer, null, matchKeyboard);
 });
 
 // bot.on(ctx => {
